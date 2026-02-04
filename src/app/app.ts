34f3160ed805f2c5f4cfb49cd1052da27591c9dc
kennedy-mr <s00271816@atu.ie>
2026-02-04 12:05:30 +0000

    Copy worksheet2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -8,5 +9,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('worksheet3');
+  protected readonly title = signal('Worksheet2 - Fav Movies App [Kennedy Moore, S00271816]');
+  protected movies=signal(
+    [
+      "Inception",
+      "The Shawshank Redemption",
+      "The Dark Knight",
+      "Pulp Fiction",
+      "Spirited Away",
+      "The Godfather"
+    ]
+  )
+
+  protected favourites=signal<string[]>([])
+  protected addToFavourites(movie: string) {
+    this.favourites.update(current => [...current, movie]);
+  }
+
+  protected deleteFromFavourites(movie: string) {
+    this.favourites.update(favourites => 
+      favourites.filter(fav => fav !== movie));
+  }
 }
